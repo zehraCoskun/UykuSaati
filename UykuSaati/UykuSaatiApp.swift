af5f5f6309d0936fa1b6a16893b65bfc48c91178
Zehra Coskun <rzg>
2023-03-08 15:49:59 +0300

    Initial Commit

---
+++ UykuSaati/UykuSaatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UykuSaatiApp.swift
+//  UykuSaati
+//
+//  Created by Zehra Coşkun on 8.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UykuSaatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
